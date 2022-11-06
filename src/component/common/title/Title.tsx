8658bd129709ba9cce7f74ed68eb64efa362542a
Shelby-Shi <alexsyk123@gmail.com>
2022-11-06 22:09:52 +1100

    change title

--- src/component/common/title/Title.tsx
+++ src/component/common/title/Title.tsx
@@ -4,8 +4,8 @@ import './Title.css';
 function Title() {
   return (
     <section className='title'>
-        <div className='title-text'>A Chinese Psychological Consultation Dialogue System
-        Based on Vary Strategies</div>
+        <h1 className='title-text'>A Chinese Psychological Consultation Dialogue System</h1>
+        <div>based on large corpus pre-training model</div>
     </section>
   )
 }
